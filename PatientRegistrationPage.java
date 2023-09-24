44d322aea34226228caa54d5caa2b0577eaba167
Ietetsec <lsupadhya@gmail.com>
2023-09-24 22:26:53 +0530

    Working timer

--- PatientRegistrationPage.java
+++ PatientRegistrationPage.java
@@ -2,9 +2,6 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 import java.util.*;
-
-// Import ArrayList and List explicitly
-import java.util.ArrayList;
 import java.util.List;
 
 class Node {
@@ -12,12 +9,14 @@ class Node {
     int priority;
     int insertionOrder;
     int agee;
+    int estimatedTime;
 
-    public Node(String data, int priority, int agee, int insertionOrder) {
+    public Node(String data, int priority, int agee, int insertionOrder, int estimatedTime) {
         this.data = data;
         this.priority = priority;
         this.agee = agee;
         this.insertionOrder = insertionOrder;
+        this.estimatedTime = estimatedTime;
     }
 }
 
@@ -34,21 +33,32 @@ public class PatientRegistrationPage {
     private int lowestpriority = 8; // Initialize lowest priority
     private int insertionOrder = 0; // Initialize insertion order
 
+    private javax.swing.Timer timer = new javax.swing.Timer(1000, new ActionListener() {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            updateEstimatedTime();
+        }
+    });
+
+    // Add a field for the countdown timer
+    private int countdown = 0;
+
     public PatientRegistrationPage() {
         frame = new JFrame("Patient Registration");
-        frame.setLayout(new GridLayout(9, 2));
+        frame.setLayout(new BorderLayout());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         // Create and add components to the frame
-        frame.add(new JLabel("Name: "));
+        JPanel formPanel = new JPanel(new GridLayout(9, 2));
+        formPanel.add(new JLabel("Name: "));
         nameField = new JTextField(20);
-        frame.add(nameField);
+        formPanel.add(nameField);
 
-        frame.add(new JLabel("Age: "));
+        formPanel.add(new JLabel("Age: "));
         ageField = new JTextField(3);
-        frame.add(ageField);
+        formPanel.add(ageField);
 
-        frame.add(new JLabel("Gender: "));
+        formPanel.add(new JLabel("Gender: "));
         maleRadioButton = new JRadioButton("Male");
         femaleRadioButton = new JRadioButton("Female");
         genderButtonGroup = new ButtonGroup();
@@ -57,24 +67,26 @@ public class PatientRegistrationPage {
         JPanel genderPanel = new JPanel();
         genderPanel.add(maleRadioButton);
         genderPanel.add(femaleRadioButton);
-        frame.add(genderPanel);
+        formPanel.add(genderPanel);
 
-        frame.add(new JLabel("Contact Info: "));
+        formPanel.add(new JLabel("Contact Info: "));
         contactField = new JTextField(20);
-        frame.add(contactField);
+        formPanel.add(contactField);
 
-        frame.add(new JLabel("Describe your illness: "));
+        formPanel.add(new JLabel("Describe your illness: "));
         illnessField = new JTextField(50);
-        frame.add(illnessField);
+        formPanel.add(illnessField);
 
-        frame.add(new JLabel("Emergency Type: "));
+        formPanel.add(new JLabel("Emergency Type: "));
         String[] emergencies = { "Select an emergency", "Cardiac Arrest", "Severe Bleeding/Trauma", "Stroke",
                 "Heart Attack(Myocardial infarction)", "Pregnancy Complications" };
         emergencyDropdown = new JComboBox<>(emergencies);
-        frame.add(emergencyDropdown);
+        formPanel.add(emergencyDropdown);
 
         submitButton = new JButton("Submit");
-        frame.add(submitButton);
+        formPanel.add(submitButton);
+
+        frame.add(formPanel, BorderLayout.NORTH);
 
         // Initialize PriorityQueue and link to PriorityQueueGUI
         priorityQueue = new PriorityQueue<>(10, new Comparator<Node>() {
@@ -91,6 +103,9 @@ public class PatientRegistrationPage {
                 submitRegistration();
             }
         });
+
+        // Start the timer
+        timer.start();
 
         frame.pack();
         frame.setVisible(true);
@@ -177,13 +192,31 @@ public class PatientRegistrationPage {
             if (priority < lowestpriority) {
                 lowestpriority = priority;
             }
-            if (priorityQueue.size() <= 10) {
+            if (priorityQueue.size() < 10) {
                 // Create a node with a composite priority value
                 // Higher priority gets higher composite value
                 int compositePriority = priority * 1000 + insertionOrder;
-                priorityQueue.offer(new Node(name, compositePriority, agee, insertionOrder++));
+                priorityQueue.offer(new Node(name, compositePriority, agee, insertionOrder++, 20)); // 20 seconds
+                int extraTime = 20; // Extra time for higher-priority node
+                int prevNodeEstTime = 0;
+                for (Node node : priorityQueue) {
+                    if (node.priority < compositePriority) {
+                        prevNodeEstTime = node.estimatedTime;
+                    } else if (node.priority == compositePriority) {
+                        node.estimatedTime = prevNodeEstTime + extraTime;
+                    } else {
+                        node.estimatedTime += extraTime;
+                    }
+                }
             } else if (priority > lowestpriority) {
                 queueGUI.appendToOutput("Emergency Spotted Please wait for 20 seconds\n");
+                // Adjust timers for nodes below
+                int extraTime = 20; // Extra time for higher-priority node
+                for (Node node : priorityQueue) {
+                    if (node.priority < priority) {
+                        node.estimatedTime += extraTime;
+                    }
+                }
             } else {
                 queueGUI.appendToOutput("Queue is full, and this node has lower priority.\n");
             }
@@ -209,14 +242,39 @@ public class PatientRegistrationPage {
             priorityQueue.addAll(sortedNodes);
 
             // Show the PriorityQueueGUI
-            queueGUI.updateQueueDisplay(priorityQueue);
+            queueGUI.updateQueueDisplay(priorityQueue, countdown);
         }
+
+    }
+
+    private void updateEstimatedTime() {
+        // Update the estimated time for each patient
+        List<Node> removedNodes = new ArrayList<>();
+        for (Node node : priorityQueue) {
+            if (node.estimatedTime > 1) {
+                node.estimatedTime--;
+            } else {
+                removedNodes.add(node);
+            }
+        }
+        // Remove nodes that reached 0 estimated time
+        priorityQueue.removeAll(removedNodes);
+
+        // Update the countdown timer
+        if (!priorityQueue.isEmpty() && countdown == 0) {
+            countdown = priorityQueue.peek().estimatedTime;
+        } else if (countdown > 0) {
+            countdown--;
+        }
+        // Update the display
+        queueGUI.updateQueueDisplay(priorityQueue, countdown);
     }
 
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
+                // Create an instance of PatientRegistrationPage
                 new PatientRegistrationPage();
             }
         });
@@ -224,11 +282,10 @@ public class PatientRegistrationPage {
 }
 
 class PriorityQueueGUI {
-    private JFrame frame;
     private JTextArea outputArea;
 
     public PriorityQueueGUI() {
-        frame = new JFrame("Priority Queue Example");
+        JFrame frame = new JFrame("Priority Queue Example");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(400, 400);
 
@@ -237,15 +294,20 @@ class PriorityQueueGUI {
 
         frame.getContentPane().setLayout(new BorderLayout());
         frame.getContentPane().add(new JScrollPane(outputArea), BorderLayout.CENTER);
-    }
-
-    public void updateQueueDisplay(PriorityQueue<Node> priorityQueue) {
-        outputArea.setText("Nodes in the Priority Queue:\n");
-        for (Node node : priorityQueue) {
-            outputArea.append("Node: " + node.data + " Patient Id: " + node.priority + " Age: " + node.agee + "\n");
-        }
 
         frame.setVisible(true);
+    }
+
+    public void updateQueueDisplay(PriorityQueue<Node> priorityQueue, int countdown) {
+        outputArea.setText("Nodes in the Priority Queue:\n");
+        for (Node node : priorityQueue) {
+            outputArea.append("Node: " + node.data + " Patient Id: " + node.priority + " Age: " + node.agee +
+                    " Estimated Time: " + node.estimatedTime + " seconds\n");
+        }
+
+        if (countdown > 0) {
+            outputArea.append("\nCountdown: " + countdown + " seconds");
+        }
     }
 
     public void appendToOutput(String text) {
